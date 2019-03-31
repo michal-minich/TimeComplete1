1b81fb1a021150e47240fea430d75589325d5560
Michal Minich <michal.minich@gmail.com>
2019-03-31 09:14:02 +0200

    udpates packages

--- src/common.ts
+++ src/common.ts
@@ -66,10 +66,10 @@ export function queryBorder(app: IApp, di: IDashItem) {
 
 export function removeTextSelections() {
     if (window.getSelection) {
-        if (window.getSelection().empty) { // Chrome
-            window.getSelection().empty();
-        } else if (window.getSelection().removeAllRanges) { // Firefox
-            window.getSelection().removeAllRanges();
+        if (window.getSelection()!.empty) { // Chrome
+            window.getSelection()!.empty();
+        } else if (window.getSelection()!.removeAllRanges) { // Firefox
+            window.getSelection()!.removeAllRanges();
         }
     }
 }
